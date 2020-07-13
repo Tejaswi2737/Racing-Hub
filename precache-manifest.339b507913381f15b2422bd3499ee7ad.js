5df66cbfa70b149a4a90fc969ce3f24f435d6c65
Tejaswi2737 <41263776+Tejaswi2737@users.noreply.github.com>
2020-07-13 22:28:43 +1000

    Updates

---
+++ precache-manifest.339b507913381f15b2422bd3499ee7ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe90509cf16122899cc03f0d32a18d81",
+    "url": "/Racing-Hub/index.html"
+  },
+  {
+    "revision": "dfdda119338ef100b9c9",
+    "url": "/Racing-Hub/static/css/2.51270f71.chunk.css"
+  },
+  {
+    "revision": "1afe7584492fc702dff6",
+    "url": "/Racing-Hub/static/css/main.2fee12d2.chunk.css"
+  },
+  {
+    "revision": "dfdda119338ef100b9c9",
+    "url": "/Racing-Hub/static/js/2.06fb2c8b.chunk.js"
+  },
+  {
+    "revision": "ec1500a8361e5007933febe1ab869ba6",
+    "url": "/Racing-Hub/static/js/2.06fb2c8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1afe7584492fc702dff6",
+    "url": "/Racing-Hub/static/js/main.18ccb5e7.chunk.js"
+  },
+  {
+    "revision": "d8a4e8042880555ef9e9",
+    "url": "/Racing-Hub/static/js/runtime-main.a5b40a89.js"
+  },
+  {
+    "revision": "4a2d5568258a6e640181d82ba4bdc451",
+    "url": "/Racing-Hub/static/media/Home.4a2d5568.gif"
+  }
+]);
